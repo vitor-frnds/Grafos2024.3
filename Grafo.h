83c56b30a60fb8a2bad22bf990ec29dea0b16be6
Henrique Marques <122735399+henriquesmarques@users.noreply.github.com>
2024-12-24 14:45:56 -0300

    Add files via upload

--- Grafo.h
+++ Grafo.h
@@ -1,11 +1,6 @@
 #ifndef GRAFO_H
 #define GRAFO_H
 
-#include <iostream>
-#include <vector>
-#include <list>
-#include <string>
-
 // Classe abstrata Grafo
 class Grafo {
 protected:
@@ -15,15 +10,16 @@ protected:
     int numVertices;
 
 public:
-    //por enquanto, coloquei todas as funções como int e sem parâmetros, 
+    //por enquanto, coloquei todas as funções como int e sem parâmetros,
     //mas depois é só trocar se precisar
+    Grafo();
     Grafo(int numVertices, bool direcionado = false, bool verticesPonderados = false, bool arestasPonderadas = false);
     //função força bruta que indica se o grafo é bipartido ou não
-    virtual int eh_bipartido() = 0;
+    virtual bool eh_bipartido() = 0;
     //- função que indica a quantidade de componentes conexas
     virtual int n_conexo() = 0;
     //- função que retorna o grau do grafo
-    virtual int get_grau(int vertice) = 0;
+    virtual int get_grau() = 0;
     //função que retorna a ordem do grafo
     virtual int get_ordem() = 0;
     //função que retorna se o grafo é direcionado ou não
@@ -43,7 +39,7 @@ public:
     //- função que lê um arquivo txt com um grafo e carrega ele
     virtual void carrega_grafo() = 0;
     //- função que lê um arquivo txt de configuracao e gera um grafo aleatorio
-    virtual void novoGrafoO() = 0;
+    virtual void novo_grafo() = 0;
 };
 
 #endif
