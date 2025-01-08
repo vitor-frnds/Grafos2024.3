c94896976b478bc4a9519646178c07955a853209
Henrique Marques <122735399+henriquesmarques@users.noreply.github.com>
2025-01-08 13:54:44 -0300

    Henrique

--- src/lista-encadeada/GrafoLista.h
+++ src/lista-encadeada/GrafoLista.h
@@ -1,33 +1,32 @@
-#ifndef GRAFO_H
-#define GRAFO_H
+#ifndef GRAFO_LISTA_H
+#define GRAFO_LISTA_H
 
+#include "../../Grafo.h"
 #include "Aresta.h"
-
-// Os grafos não devem aceitar arestas múltiplas ou laços
-// Uso de lista encadeada tanto para vertices quanto arestas usando alocação dinâmica
-// Aresta armazena 1 quando não é ponderada
 
 class Vertice;
 
-class GrafoLista {
+class GrafoLista : public Grafo {
 public:
+    // Henrique
     GrafoLista();
     ~GrafoLista();
-    void carrega_grafo(); /// Função que lê um arquivo txt com um grafo e carrega ele
-    void novo_grafo(); /// Função que lê um arquivo txt de configuração e gera um grafo aleatório
-    bool aresta_ponderada(); /// Função que informa se as arestas do grafo tem peso
-    int get_ordem(); /// Função que retorna a ordem do grafo
-    bool eh_direcionado(); /// Função que retorna se o grafo é direcionado ou não
-    bool eh_arvore(); /// Função que diz se o grafo é uma árvore
-    int n_conexo(); /// Função que indica a quantidade de componentes conexas
-
-    bool eh_completo(); /// Função que diz se o grafo é completo ou não
-    int get_grau(); /// Função que retorna o grau do grafo
-    bool eh_bipartido(); /// Função que diz se o grafo é bipartido ou não
-    bool possui_articulacao(); /// Função que diz se possui vertice de articulação
-    bool possui_ponte(); /// Função que diz se possui aresta ponte
-    bool vertice_ponderado(); /// Função que diz se o no possui peso
+    void carrega_grafo() override; /// Função que lê um arquivo txt com um grafo e carrega ele
+    void novo_grafo() override; /// Função que lê um arquivo txt de configuração e gera um grafo aleatório
+    bool aresta_ponderada() override; /// Função que informa se as arestas do grafo tem peso
+    int get_ordem() override; /// Função que retorna a ordem do grafo
+    bool eh_direcionado() override; /// Função que retorna se o grafo é direcionado ou não
+    bool eh_arvore() override; /// Função que diz se o grafo é uma árvore
+    int n_conexo() override; /// Função que indica a quantidade de componentes conexas
+    // Karine
+    bool eh_completo() override; /// Função que diz se o grafo é completo ou não
+    int get_grau() override; /// Função que retorna o grau do grafo
+    bool eh_bipartido() override; /// Função que diz se o grafo é bipartido ou não
+    bool possui_articulacao() override; /// Função que diz se possui vertice de articulação
+    bool possui_ponte() override; /// Função que diz se possui aresta ponte
+    bool vertice_ponderado() override; /// Função que diz se o no possui peso
 private:
+    // Henrique
     Vertice* raizVertice;
     Aresta* raizAresta;
     bool direcionado;
@@ -43,7 +42,7 @@ private:
     int sortearPeso(int n);
     Aresta* inserirArestaAleatoria(int ordem, int peso);
     void dividirVertices(Vertice **grupo1, Vertice **grupo2);
-
+    // Karine
     Aresta* buscaAresta(int id, int peso);
     Vertice* buscaVertice(int id);
     GrafoLista* copiarGrafo();
@@ -51,4 +50,4 @@ private:
     void removerAresta(Aresta* aresta);
 };
 
-#endif //GRAFO_H
+#endif //GRAFO_LISTA_H
